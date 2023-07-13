1a29190eeba1ead8126ec9dee0de103233de6d5f
Donn <me@donn.website>
2023-07-13 10:20:04 +0300

    Update SYNTH_MAX_FANOUT

--- xtea/sky130A_sky130_fd_sc_hs_config.tcl
+++ xtea/sky130A_sky130_fd_sc_hs_config.tcl
@@ -1,5 +1,5 @@
 # SCL Configs
-set ::env(SYNTH_MAX_FANOUT) 6
+set ::env(MAX_FANOUT_CONSTRAINT) 6
 set ::env(FP_CORE_UTIL) 35
 
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
