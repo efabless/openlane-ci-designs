0150e998ea2db42c5380e871206535a4aa8e9d5a
kareefardi <kareem.farid@efabless.com>
2023-08-02 17:10:58 +0300

    Enable more aggressive antenna fix config for xtea

--- xtea/sky130A_sky130_fd_sc_hd_config.tcl
+++ xtea/sky130A_sky130_fd_sc_hd_config.tcl
@@ -4,3 +4,5 @@ set ::env(FP_CORE_UTIL) 45
 set ::env(MAX_FANOUT_CONSTRAINT) 6
 set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
 
+set ::env(RUN_HEURISTIC_DIODE_INSERTION) 1
+set ::env(DIODE_ON_PORTS) "in"
